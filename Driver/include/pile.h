2e36317f7cff598975ed7724d4af64431523b861
F4r3n <guillaume_k@hotmail.fr>
2014-06-06 12:24:58 +0200

    Doc

--- Driver/include/pile.h
+++ Driver/include/pile.h
@@ -1,3 +1,7 @@
+/**
+  *\file pile.h
+  */
+
 #ifndef PILE_H
 #define PILE_H
 
@@ -20,9 +24,35 @@ typedef struct _Pile
 	int taille;
 }Pile;
 
+/**
+  *\fn int videPile(Pile *positionsPile);
+  *\brief permet de savoir si la pile est vide
+  *\param La pile
+  *\return 1 si vide 0 sinon
+  */
 int videPile(Pile *positionsPile);
+
+/**
+  *\fn void empiler(Pile *positionsPile, ElementFile b);
+  *\brief permet d'empiler un élément sur la pile
+  *\param la pile et l'élemnt à empiler 
+  *\return void
+  */
 void empiler(Pile *positionsPile, ElementFile b);
+
+/**
+  *\fn ElementFile depiler(Pile *positionsPile);
+  *\brief permet de défiler un élément de la pile
+  *\param La pile
+  *\return l'élément dépilé
+  */
 ElementFile depiler(Pile *positionsPile);
+
+/**
+  *\fn Pile *creerPile()
+  *\brief Permet de créer la pile
+  *\return la pile
+  */
 Pile *creerPile();
 
 #endif
